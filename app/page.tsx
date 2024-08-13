74179614ec29009b374a2ca204cf07ce16a9d8fc
Arjun kumar vs <arjun.tech177@gmail.com>
2024-08-13 20:12:43 +0530

    Added hero sectin and shadow effects

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
+import Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
   <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
     <div className="mx-w-7xl w-full">
-      <h1>Hello Protfolio</h1>
+     <Hero />
     </div>
   </main>
   );
