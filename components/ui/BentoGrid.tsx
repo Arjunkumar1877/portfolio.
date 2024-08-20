02af6ec7bb58f1fa6744d222a6611ad0319cb0e5
Arjun kumar vs <arjun.tech177@gmail.com>
2024-08-21 02:51:52 +0530

    Added a github Globe from Aceternity ui

--- components/ui/BentoGrid.tsx
+++ components/ui/BentoGrid.tsx
@@ -1,5 +1,6 @@
 import { cn } from "@/lib/utils";
 import { BackgroundGradientAnimation } from "./GradientBg";
+import { GlobeDemo } from "./GridGlobe";
 
 export const BentoGrid = ({
   className,
@@ -44,7 +45,7 @@ export const BentoGridItem = ({
   return (
     <div
       className={cn(
-        "row-span-1 rounded-3xl relative group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
+        "row-span-1 rounded-3xl relative overflow-hidden group/bento hover:shadow-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:border-white/[0.2] bg-white border border-transparent justify-between flex flex-col space-y-4 border-red-500",
         className
       )}
       style={{
@@ -95,24 +96,12 @@ export const BentoGridItem = ({
             {title}
           </div>
         </div>
+
+{
+  id===2 && <GlobeDemo />
+}
+
       </div>
-
-      {/* <div className="group-hover/bento:translate-x-2 transition duration-200">
-      
-        <div className="font-sans font-normal text-neutral-600 text-xs dark:text-neutral-300">
-          {description}
-        </div>
-
-        <div className="font-sans font-bold text-lg text-neutral-600 dark:text-neutral-200 mb-2 mt-2">
-          {title}
-        </div>
-      </div> */}
-
-      {/* <div className="group-hover/bento:translate-x-2 transition duration-200">
-  {
-    
-  }
-</div> */}
     </div>
   );
 };
