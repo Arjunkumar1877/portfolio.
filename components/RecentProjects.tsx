ebc3cd045844d12a9669fd7b573af9d1e99506b4
Arjun kumar vs <arjun.tech177@gmail.com>
2024-08-23 04:39:00 +0530

    fixed flex wrap issue fo recent projects cards

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -13,7 +13,7 @@ const RecentProjects = () => {
      <div className="flex flex-wrap items-center justify-center p-4 gap-x-24 gap-y-8 mt-10">
       {
         projects.map(({id, title, des, img, iconLists, link})=> (
-          <div className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem]  flex items-center justify-center sm:w-[570px] w[80vw]" key={id}>
+          <div className="sm:h-[41rem] h-[32rem] lg:min-h-[32.5rem]  flex items-center justify-center sm:w-[570px] w-[80vw]" key={id}>
             { 
              <PinContainer title={link} href={link} >
              <div className="relative flex items-center justify-center sm:w-[570px] w-[80vw] overflow-hidden sm:h-[40vh] h-[30vh]  mb-10">
