780e8e45c0f7aeabc66024ad02e293070ded1891
Arjun kumar vs <arjun.tech177@gmail.com>
2024-08-31 02:35:09 +0530

    floating nav mobile responsive display issu fixed

--- components/ui/FloatingNav.tsx
+++ components/ui/FloatingNav.tsx
@@ -1,3 +1,6 @@
+
+
+
 "use client";
 import React, { useState } from "react";
 import {
@@ -25,7 +28,6 @@ export const FloatingNav = ({
   const [visible, setVisible] = useState(false);
 
   useMotionValueEvent(scrollYProgress, "change", (current) => {
-    // Check if current is not undefined and is a number
     if (typeof current === "number") {
       let direction = current! - scrollYProgress.getPrevious()!;
 
@@ -56,7 +58,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed top-10 inset-x-0 mx-auto border rounded-lg  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-10 py-5   items-center justify-center space-x-4 border-white/[0.2] bg-black-100",
+          "flex max-w-fit fixed top-10 inset-x-0 mx-auto border rounded-lg shadow z-[5000] px-10 py-5 items-center justify-center space-x-4 border-white/[0.2] bg-black-100",
           className
         )}
       >
@@ -65,14 +67,15 @@ export const FloatingNav = ({
             key={`link=${idx}`}
             href={navItem.link}
             className={cn(
-              "relative dark:text-neutral-50 items-center flex space-x-1 text-neutral-600 dark:hover:text-neutral-300 hover:text-neutral-500"
+              "relative text-center flex flex-col items-center space-x-1 text-neutral-600 dark:text-neutral-50 dark:hover:text-neutral-300 hover:text-neutral-500"
             )}
           >
-            <span className="block sm:hidden">{navItem.icon}</span>
-            <span className="hidden sm:block text-sm">{navItem.name}</span>
+            {/* Show icon always */}
+            <span className="block">{navItem.icon}</span>
+            {/* Show text on all screen sizes */}
+            <span className="block text-xs sm:text-sm">{navItem.name}</span>
           </Link>
         ))}
-       
       </motion.div>
     </AnimatePresence>
   );
